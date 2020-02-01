50215ad5d19c98bf4c1c27ebd61ed66feeb48de2
jebrax <batonez@gmail.com>
2020-02-01 17:01:23 +0300

    Added skeleton animation controllers

---
+++ RePair/Assets/Code/SkeletonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class SkeletonController : MonoBehaviour
+{
+    public void SwitchAnimationState(string animState)
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++) {
+            GameObject child = gameObject.transform.GetChild(i).gameObject;
+            SkeletonPartController skel = child.GetComponent<SkeletonPartController>();
+            skel.PlayAnimation(animState);
+        }
+    }
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchAnimationState("walk");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RePair/Assets/Code/SkeletonPartController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Spine.Unity;
+
+public class SkeletonPartController : MonoBehaviour
+{
+    public SkeletonDataAsset skeleton;
+    
+    public void PlayAnimation(string anim)
+    {
+        SkeletonAnimation skeletonAnimation = gameObject.GetComponent<SkeletonAnimation>();
+        if (skeletonAnimation != null) {
+            skeletonAnimation.state.SetAnimation(0, anim, true);
+            for (int i = 0; i < gameObject.transform.childCount; i++) {
+                GameObject child = gameObject.transform.GetChild(i).gameObject;
+                SkeletonPartController skel = child.GetComponent<SkeletonPartController>();
+                skel.PlayAnimation(anim);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SkeletonAnimation skeletonAnimation = gameObject.AddComponent<SkeletonAnimation>();
+        skeletonAnimation.skeletonDataAsset = skeleton;
+        skeletonAnimation.Initialize(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
