5d41493ca23a8d9ef96f0661a21b0e85d380dfc1
jebrax <batonez@gmail.com>
2020-02-02 12:54:00 +0300

    Particles affected by the wind

--- RePair/Assets/Code/WindAffected.cs
+++ RePair/Assets/Code/WindAffected.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class WindAffected : MonoBehaviour
@@ -5,7 +6,7 @@ public class WindAffected : MonoBehaviour
 	void Start()
 	{
 		Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
-        if (rigidbody) {
+        if (rigidbody != null) {
             GameController game = GameController.GetInstance();
             if (game) {
                 Wind windComponent = game.GetComponent<Wind>();
@@ -14,4 +15,28 @@ public class WindAffected : MonoBehaviour
             }
         }
 	}
+
+	void Update()
+	{
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        if (particleSystem != null) {
+            GameController game = GameController.GetInstance();
+            if (game) {
+                Wind windComponent = game.GetComponent<Wind>();
+                if (windComponent != null) {
+                    float windX = windComponent.GetWindForce().x;
+                    float windMag = Math.Abs(windX);
+                    float windDir = windX/windMag;
+                    float horizontalSpeed = (windMag - 0.0f) / (80.0f - 0.0f) * (4.0f - 0.0f) + 0.0f;
+
+                    var psProps = particleSystem.main;
+                    float simulationSpeed = (windMag - 0.0f) / (80.0f - 0.0f) * (4.0f - 1.0f) + 1.0f;
+                    psProps.simulationSpeed = simulationSpeed;
+                    
+                    float startSpeed = UnityEngine.Random.Range(horizontalSpeed, horizontalSpeed);
+                    psProps.startSpeed = startSpeed * windDir;
+                }
+            }
+        }
+    }
 }
