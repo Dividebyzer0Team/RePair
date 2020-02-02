c71c73547cd868592e2d4709a4f7f2d2e738f322
Stranger <dmitriy.troitskiy@gmail.com>
2020-02-02 08:38:57 +0300

    Resolved merge conflict and adjusted tabs.

--- RePair/Assets/Code/Animal/Animal.cs
+++ RePair/Assets/Code/Animal/Animal.cs
@@ -22,18 +22,7 @@ public class Animal : MonoBehaviour
 	float m_orientation; // +1 = right
 	float m_hungerFactor;
 	bool m_active;
-<<<<<<< HEAD
-=======
-    bool m_dead;
-	enum MovementMethod
-	{
-		WALK,
-		JUMP,
-		FLY
-	}
-
-
->>>>>>> 853900d3faedf14f04eba03549c225e2f92fc122
+	bool m_dead;
 	MovementMethod m_movementMethod = MovementMethod.WALK;
 
 	public Genome GetGenome()
@@ -63,9 +52,9 @@ public class Animal : MonoBehaviour
 			m_movementMethod = MovementMethod.FLY;
 			m_rigidbody.gravityScale = 0f;
 		}
-        m_traits["age"] = 0f;
-        m_traits["currentSize"] = GetTrait("size") * 0.5f;
-        float size = GetTrait("size") * 0.5f;
+		m_traits["age"] = 0f;
+		m_traits["currentSize"] = GetTrait("size") * 0.5f;
+		float size = GetTrait("size") * 0.5f;
 		m_view.SetBodyPart("Head", m_genome.Head.animalName);
 		m_view.SetBodyPart("Front", m_genome.Body.animalName);
 		m_view.SetBodyPart("Rear", m_genome.Legs.animalName);
@@ -80,41 +69,41 @@ public class Animal : MonoBehaviour
 		}
 	}
 
-    public bool IsDead()
-    {
-        return m_dead;
-    }
-
-    public void Die()
+	public bool IsDead()
 	{
-        m_rigidbody.velocity = Vector2.zero;
-        m_dead = true;
-        GetComponent<Collider2D>().enabled = false;
-        m_rigidbody.gravityScale = 0;
-        m_behaviour = new Die(this);
-        Invoke("DestroySelf", 10f);
+		return m_dead;
+	}
+
+	public void Die()
+	{
+		m_rigidbody.velocity = Vector2.zero;
+		m_dead = true;
+		GetComponent<Collider2D>().enabled = false;
+		m_rigidbody.gravityScale = 0;
+		m_behaviour = new Die(this);
+		Invoke("DestroySelf", 10f);
 		//Destroy(gameObject);
 	}
 
-    void ManageAge(float deltaTime)
-    {
-        m_traits["age"] += deltaTime;
-        float age = GetTrait("age");
-        float size = GetTrait("size");
+	void ManageAge(float deltaTime)
+	{
+		m_traits["age"] += deltaTime;
+		float age = GetTrait("age");
+		float size = GetTrait("size");
 
-        float lifeProgess = age / GetTrait("lifetime");
-        float sizeMod = Mathf.Min(1f, 0.5f + lifeProgess * 2.5f);
-        m_traits["currentSize"] = sizeMod * size;
-        float currentSize = sizeMod * size;
-        transform.localScale = new Vector3(m_orientation * currentSize, currentSize, 1f);
-    }
+		float lifeProgess = age / GetTrait("lifetime");
+		float sizeMod = Mathf.Min(1f, 0.5f + lifeProgess * 2.5f);
+		m_traits["currentSize"] = sizeMod * size;
+		float currentSize = sizeMod * size;
+		transform.localScale = new Vector3(m_orientation * currentSize, currentSize, 1f);
+	}
 
-    void DestoySelf()
-    {
-        Destroy(gameObject);
-    }
+	void DestoySelf()
+	{
+		Destroy(gameObject);
+	}
 
-    void Awake()
+	void Awake()
 	{
 		m_animalBase = GameObject.Find("GameController").GetComponent<GameController>().animalBase;
 		m_view = GetComponentInChildren<SkeletonController>();
@@ -124,20 +113,20 @@ public class Animal : MonoBehaviour
 		Invoke("Idle", 0.3f);
 	}
 
-    void UpdateSize()
-    {
-
-    }
-
-    void FixedUpdate()
+	void UpdateSize()
 	{
-        if (!m_active)
-            return;
-        if (m_behaviour != null) m_behaviour.Update(Time.fixedDeltaTime);
-        m_orientation = Mathf.Sign(-m_rigidbody.velocity.x);
-        if (m_orientation == 0f)
-            m_orientation = 1f;
-        ManageAge(Time.fixedDeltaTime);
+
+	}
+
+	void FixedUpdate()
+	{
+		if (!m_active)
+			return;
+		if (m_behaviour != null) m_behaviour.Update(Time.fixedDeltaTime);
+		m_orientation = Mathf.Sign(-m_rigidbody.velocity.x);
+		if (m_orientation == 0f)
+			m_orientation = 1f;
+		ManageAge(Time.fixedDeltaTime);
 		// hunger
 		if (m_traits.ContainsKey("stomachSize"))
 		{
@@ -209,7 +198,7 @@ public class Animal : MonoBehaviour
 			behaviorSelected = true;
 		}
 		if (!behaviorSelected && m_traits.ContainsKey("running") && m_traits.ContainsKey("panic") &&
-		    Random.value < m_traits["panic"])
+				Random.value < m_traits["panic"])
 		{
 			m_behaviour = new Run(this);
 			behaviorSelected = true;
@@ -227,21 +216,21 @@ public class Animal : MonoBehaviour
 
 	public void Mate(Animal other)
 	{
-        other.Idle();
-        GameObject newAnimalGO = Instantiate(GameController.GetInstance().animalBase);   
-        newAnimalGO.transform.position = (transform.position + other.transform.position) / 2;
-        Animal newAnimal = newAnimalGO.GetComponent<Animal>();
-        newAnimal.Inherit(this, other);
-        PushParent(other, newAnimal);
-        PushParent(this, newAnimal);
-    }
+		other.Idle();
+		GameObject newAnimalGO = Instantiate(GameController.GetInstance().animalBase);	 
+		newAnimalGO.transform.position = (transform.position + other.transform.position) / 2;
+		Animal newAnimal = newAnimalGO.GetComponent<Animal>();
+		newAnimal.Inherit(this, other);
+		PushParent(other, newAnimal);
+		PushParent(this, newAnimal);
+	}
 
-  public void PushParent(Animal parent, Animal child)
-  {
-      parent.GetRigidbody().velocity = Vector2.zero;
-      Vector3 force = new Vector2(Mathf.Sign(parent.transform.position.x - child.transform.position.x), 1f) * 100f;
-      parent.GetRigidbody().AddForce(force);
-  }
+	public void PushParent(Animal parent, Animal child)
+	{
+		parent.GetRigidbody().velocity = Vector2.zero;
+		Vector3 force = new Vector2(Mathf.Sign(parent.transform.position.x - child.transform.position.x), 1f) * 100f;
+		parent.GetRigidbody().AddForce(force);
+	}
 
 	public Rigidbody2D GetRigidbody()
 	{
