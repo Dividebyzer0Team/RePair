877cc04f7c173b786133b91b322e0820e4132d01
Zhimkov <zhimkov@game-insight.com>
2020-02-02 17:06:13 +0300

    UI base logic

---
+++ RePair/Assets/TextInit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().sortingOrder = 9002;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RePair/Assets/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	public GameObject logo;
+	public GameObject tutorialManager;
+	public GameObject badEnding;
+	public GameObject goodEnding;
+	public GameObject restartText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
