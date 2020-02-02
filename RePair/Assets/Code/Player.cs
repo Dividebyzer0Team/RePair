9368eb410e3c7d2413cf146781e3bf02d26b8368
Stranger <dmitriy.troitskiy@gmail.com>
2020-02-02 17:15:44 +0300

    Charge effect added.

--- RePair/Assets/Code/Player.cs
+++ RePair/Assets/Code/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour
 {
 	public GameObject loveArrowPrefab;
 	public GameObject warArrowPrefab;
+	public GameObject arrowChargeEffect;
 	public GameObject matingReadyEffect;
 	public float arrowForceFactor = 1.0f;
 	public float chargeForceFactor = 1.0f;
@@ -13,6 +14,7 @@ public class Player : MonoBehaviour
 
 	private Animal m_queuedAnimal;
 	private float m_shotCharge, m_shotChargeTime;
+	private GameObject m_curArrowChargeEffect;
 	private GameObject m_view;
 	private SkeletonAnimation m_animation;
 	private Transform m_aim;
@@ -58,8 +60,17 @@ public class Player : MonoBehaviour
 		}
 		else
 		{
+			if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+			{
+				if (m_curArrowChargeEffect) Destroy(m_curArrowChargeEffect);
+				m_curArrowChargeEffect = Instantiate(arrowChargeEffect, transform);
+			}
 			if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
 			{
+				var toMouse = (worldMousePosition - transform.position).normalized;
+				var rotation = new Vector3(0, 0, -Vector2.SignedAngle(toMouse.normalized, Vector2.right));
+				m_curArrowChargeEffect.transform.rotation = Quaternion.Euler(rotation);
+				
 				m_shotChargeTime += Time.deltaTime;
 				if (m_shotChargeTime <= chargeMaxTime)
 				{
@@ -68,6 +79,9 @@ public class Player : MonoBehaviour
 			}
 			if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
 			{
+				Destroy(m_curArrowChargeEffect);
+				m_curArrowChargeEffect = null;
+
 				m_animation.AnimationState.SetAnimation(0, "shoot", false);
 				m_animation.AnimationState.AddAnimation(0, "prepare", false, 0);
 				m_animation.AnimationState.AddAnimation(0, "idle", true, 0);
